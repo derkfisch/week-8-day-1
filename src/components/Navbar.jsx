870e6c3d16af32ead4aae698f03f29e9d7f3b8d1
derkfisch <dlf418@gmail.com>
2023-04-04 23:23:54 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+
+    return (
+        <nav className="navbar bg-body-success">
+            <div className="container-fluid">
+                <a href="/" className="navbar-brand">To Do App</a>
+            </div>
+        </nav>
+    )
+}
